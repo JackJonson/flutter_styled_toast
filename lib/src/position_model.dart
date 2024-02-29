a3c0ceaf9d538168831acb5de2c9d3519913cbc3
christo pb <christo@ateamsoftsolutions.com>
2024-02-29 20:59:22 +0530

    Added model class for params in Position widget

---
+++ lib/src/position_model.dart
@@ -0,0 +1,17 @@
+class OverlayPosition {
+  final double? left;
+  final double? top;
+  final double? right;
+  final double? bottom;
+  final double? width;
+  final double? height;
+
+  const OverlayPosition({
+    this.left,
+    this.top,
+    this.right,
+    this.bottom,
+    this.width,
+    this.height,
+  });
+}
